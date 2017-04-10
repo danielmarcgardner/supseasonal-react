8c49cdb54e4b02b80f5e99fdc53498449184e5a3
Daniel Gardner <daniel.marc.gardner@gmail.com>
2017-04-10 09:37:35 -0700

    pushing up commit

--- src/components/app-maybe.js
+++ src/components/app-maybe.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import IndexPage from './home/home'
-// import IngredientAvail from './ingredients/ingredient_availability_table'
+import IngredientAvail from './ingredients/ingredient_availability_table'
 
 export default class App extends Component {
   render() {
     return (
-        <IndexPage /> );
+        <IndexPage />
+      );
   }
 }
